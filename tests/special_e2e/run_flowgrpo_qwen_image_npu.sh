8971da8277d9fdf3dc8492367717b74d7acd1e1f
ChibiQuest <mayimei5@huawei.com>
2026-09-07 16:16:30 +0800

    Remove use_shm parameter from model config
    Removed the use_shm parameter from the model configuration.
    
    Signed-off-by: ChibiQuest <mayimei5@huawei.com>

--- tests/special_e2e/run_flowgrpo_qwen_image_npu.sh
+++ tests/special_e2e/run_flowgrpo_qwen_image_npu.sh
@@ -38,7 +38,6 @@ python3 -m verl_omni.trainer.main_diffusion \
     data.max_prompt_length=${max_prompt_length} \
     actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.model.tokenizer_path=${TOKENIZER_PATH} \
-    actor_rollout_ref.model.use_shm=True \
     actor_rollout_ref.model.lora_rank=8 \
     actor_rollout_ref.model.lora_alpha=16 \
     actor_rollout_ref.model.target_modules=all-linear \
