831c5ef7ddbca5fb14711d76c30dc4905cee61df
ChibiQuest <mayimei5@huawei.com>
2026-09-07 14:12:12 +0800

    Update MODEL_PATH to include snapshots directory
    Signed-off-by: ChibiQuest <mayimei5@huawei.com>

--- tests/special_e2e/run_flowgrpo_qwen_image_npu.sh
+++ tests/special_e2e/run_flowgrpo_qwen_image_npu.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 NUM_NPUS=${NUM_NPUS:-4}
-MODEL_PATH=${MODEL_PATH:-${HOME}/.cache/huggingface/hub/models--Qwen--Qwen-Image}
+MODEL_PATH=${MODEL_PATH:-${HOME}/.cache/huggingface/hub/models--Qwen--Qwen-Image/snapshots}
 TOKENIZER_PATH=${TOKENIZER_PATH:-${MODEL_PATH}/tokenizer}
 DATA_DIR=${DATA_DIR:-${HOME}/.cache/datasets/ocr}
 dummy_train_path=${TRAIN_FILES:-${DATA_DIR}/train.parquet}
